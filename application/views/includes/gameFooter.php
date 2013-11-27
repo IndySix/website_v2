2567d50b53c53754ba1bd2a2d330e4de57f392bc
Krukas <maikel_martens_1@hotmail.com>
2013-11-27 14:13:01 +0100

    Moved images and fonts

--- application/views/includes/gameFooter.php
+++ application/views/includes/gameFooter.php
@@ -1,29 +1,31 @@
 		</div>
 		<div id="settings">
-			
+			<a href="<?PHP echo baseUrl('user/logout') ?>">Logout</a>
 		</div>
 	</div>
+	<?PHP if ($this->ModelLogin->isLoggedin()): ?>
 	<div id="bottomBar">
 		<div id="menu">
 
 			<a id='playButton' href="#"><span class="playIcon"></span></a>
 			<a class="left" href="#">
-				<img src="<?PHP echo baseUrl('data/game/images/career.png') ?>"><br>
+				<img src="<?PHP echo baseUrl('data/img/career.png') ?>"><br>
 				Career
 			</a>
 			<a class="left" href="#">
-				<img src="<?PHP echo baseUrl('data/game/images/battle.png') ?>"><br>
+				<img src="<?PHP echo baseUrl('data/img/battle.png') ?>"><br>
 				Battles
 			</a>
 			<a class="right" href="#">
-				<img src="<?PHP echo baseUrl('data/game/images/video.png') ?>"><br>
+				<img src="<?PHP echo baseUrl('data/img/video.png') ?>"><br>
 				Video
 			</a>
 			<a class="right" href="#">
-				<img src="<?PHP echo baseUrl('data/game/images/ranks.png') ?>"><br>
+				<img src="<?PHP echo baseUrl('data/img/ranks.png') ?>"><br>
 				Ranks
 			</a>
 		</div>
 	</div>
+	<?PHP endif; ?>
 </body>
 </html>
