45acaa2ac031d9f5e1b69db20b4196f7139d60d8
Krukas <maikel_martens_1@hotmail.com>
2013-11-27 14:07:56 +0100

    add friends page #temp

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,14 +3,23 @@
 class Controller_User extends Core_Controller {
    
   	function index(){
-      if($this->ModelLogin->isLoggedin())
-        $this->view();
+      if( $this->ModelApp->isGame() ){
+         if($this->ModelLogin->isLoggedin())
+            $this->register();
          else
-        $this->register();
+            $this->view();
+      } else {
+         if($this->ModelLogin->isLoggedin())
+            $this->view();
+         else
+            $this->register();
+      }
    }
 
    function friends(){
-      $this->load->view('friendsView');
+      $this->load->model('User');
+      $data['friends'] = $this->ModelUser->getFriends($this->LibSession->get('user_id'));
+      $this->load->view('friendsView', $data);
    }
    function battles(){
       $this->load->view('battlesView');
@@ -100,7 +109,7 @@ class Controller_User extends Core_Controller {
 
    function logout(){
       $this->ModelLogin->logout();
-      $this->register();
+      $this->index();
    }
 
    function register(){
@@ -179,7 +188,7 @@ class Controller_User extends Core_Controller {
       elseif( !$_registerSuccessful)
          $this->loadView('userRegister', $data);
       else
-         redirect("home");
+         redirect("user");
    }
 
    function validateEmail(){
--- application/views/friendsView.php
+++ application/views/friendsView.php
@@ -1,3 +1,14 @@
-
-<h1>Friends</h1>
-List friends and maybe status updates here?
+<?PHP foreach($friends as $u): 
+	if($u['user_id'] == $this->LibSession->get('user_id'))
+		$user = $this->ModelUser->byId( $u['friend_id'] );
+	else
+		$user = $this->ModelUser->byId( $u['user_id'] );
+?>
+<p>
+	<a href="<?PHP echo baseUrl('user/view/'.$user['id'].'/'.$user['username']) ?>">
+		
+			<img src="<?PHP echo baseUrl('data/avatars/'.$user['avatar']) ?>" width="60px">
+			<?PHP echo $user['username'] ?>
+	</a>
+  </p>
+<?PHP endforeach; ?>
